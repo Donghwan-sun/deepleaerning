3b1204f9386f0a6a7bff0d65c596f000c5bda3b5
선동환 <sunehd564@gmail.com>
2020-09-28 10:13:43 +0900

    gradient_descent 구현

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 import numpy
 
-x =
+x = [1, 2, 3]
+y = [1, 2, 3]
+W = tf.Variable(tf.random.normal([1]), name="weight")
+b = tf.Variable(tf.random.normal([1]), name="bias ")
+learning_rate = 0.1
+gradient = tf.reduce_mean((W * x - y) * x)
+descent = W - learning_rate * gradient
+update = W.assign(descent)
+print(update)
